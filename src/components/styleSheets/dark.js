f1429d6a33d372e774395ab8f395b324b2c41c2d
Hasan Ozovali <ozovalihasan@gmail.com>
2021-03-22 21:54:30 +0300

    Update theme styles

--- src/components/styleSheets/dark.js
+++ src/components/styleSheets/dark.js
@@ -6,6 +6,11 @@ const darkTheme = {
   instagram: 'linear gradient hsl(37, 97%, 70%) to hsl(329, 70%, 58%)',
   youTube: 'hsl(348, 97%, 39%)',
   toggle: 'linear-gradient(hsl(210, 78%, 56%), hsl(146, 68%, 55%))',
+  backgroundTheme: 'hsl(230, 17%, 14%)',
+  topBackgroundTheme: 'hsl(232, 19%, 15%)',
+  cardBackground: 'hsl(228, 28%, 20%)',
+  textFirst: 'hsl(228, 34%, 66%)',
+  textSecond: 'hsl(0, 0%, 100%)',
 };
 
 export default darkTheme;
--- src/components/styleSheets/light.js
+++ src/components/styleSheets/light.js
@@ -6,6 +6,11 @@ const lightTheme = {
   instagram: 'linear gradient hsl(37, 97%, 70%) to hsl(329, 70%, 58%)',
   youTube: 'hsl(348, 97%, 39%)',
   toggle: 'hsl(230, 22%, 74%)',
+  backgroundTheme: 'hsl(0, 0%, 100%)',
+  topBackgroundTheme: 'hsl(225, 100%, 98%)',
+  cardBackground: 'hsl(227, 47%, 96%)',
+  textFirst: 'hsl(228, 12%, 44%)',
+  textSecond: 'hsl(230, 17%, 14%)',
 };
 
 export default lightTheme;
